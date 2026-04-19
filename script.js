f26260ebd8a74dabb783e1f8c971d7ebd9dc6a35
76144 <kpietra5@stu.vistula.edu.pl>
2026-04-19 23:35:15 +0000

    Update from https://github.com/76144/cv-76144/commit/8d75ccb8d2d8f1ac8ce04b797afde4b04112df86

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// 1. ZMIANA MOTYWU
 const btnTheme = document.getElementById('btn-theme');
 const themeLink = document.getElementById('theme-link');
 
 btnTheme.addEventListener('click', function() {
-    // Sprawdzamy, jaki arkusz jest aktualnie podpięty
     if (themeLink.getAttribute('href') === 'red.css') {
-        themeLink.setAttribute('href', 'green.css'); // Zmień na zielony
+        themeLink.setAttribute('href', 'green.css');
     } else {
-        themeLink.setAttribute('href', 'red.css'); // Wróć do czerwonego
+        themeLink.setAttribute('href', 'red.css');
     }
 });
 
-// 2. UKRYWANIE I POKAZYWANIE SEKCJI PROJEKTY
 const btnToggleProjects = document.getElementById('btn-toggle-projects');
 const sectionProjects = document.getElementById('sekcja-projekty');
 
 btnToggleProjects.addEventListener('click', function() {
-    // Jeśli sekcja jest ukryta (display: none), to ją pokaż. Jeśli widoczna, to ukryj.
     if (sectionProjects.style.display === 'none') {
         sectionProjects.style.display = 'block';
     } else {
